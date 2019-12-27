df7e08367309cc8954cb47fb17032d87dc952672
Ousman <ordiallo@gmail.com>
2019-12-27 15:13:28 -0500

    update route visualiztion with circle divs and hover effect

--- app/charts/route-trace.jsx
+++ app/charts/route-trace.jsx
@@ -6,17 +6,6 @@ const RouteLocations = (props) => {
   const communicationsData = useContext(CommunicationsContext).overviewData;
 
   console.log('THIS IS ROUTE TRACE: ', communicationsData);
-
-  /*
-    correlatingid: "f1f1acd9-0613-4c9a-bb46-40366389ae5d"
-    currentmicroservice: "goalServer"
-    id: 1
-    reqtype: "GET"
-    resmessage: "OK"
-    resstatus: 200
-    targetedendpoint: "/user/getuser/jenae"
-    timesent: "2019-12-20T03:07:09.655Z"
-  */
     
   const resObj = {};
   for (let i = 0; i < communicationsData.length; i+=1) {
@@ -33,7 +22,7 @@ const RouteLocations = (props) => {
   for (let i = 0; i < tracePoints[0].length; i+=1) {
     resArray.push(
         <div className="RouteBox" key={i}>
-        <p>{tracePoints[0][i]}</p>
+        <p id="routeText">Point {i+1}: {tracePoints[0][i]}</p>
         </div>
     )
   };
